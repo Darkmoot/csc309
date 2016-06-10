88bc59adb0127aed4ff9e1852bedfa3a87eed14e
g2alla <alla.guba@yahoo.ca>
2016-06-10 13:46:48 -0400

    Making sure I have latest copy.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,5 @@
 window.onload = function() {
+    debugger;
     
     /* Global variables */
     
@@ -6,8 +7,8 @@ window.onload = function() {
     window.ingame_time = 60;
     window.game_level = 0;
     window.game_score = 200;
-    
-    /* Global Methods */
+    // Array which stores the functions to be drawn
+    window.drawings = [moon, planet, spaceship];
 
     
 // Store and retrieve high score to/from local storage.
@@ -35,6 +36,17 @@ if (typeof(Storage) !== "undefined") {
     
 }
 
+/* Global Methods */
+// Draw a new planet, spaceship, and moon every 1s
+    
+function drawSpaceObjects(){
+    for(i = 0; i < 10; i++) {
+        var x = Math.floor((Math.random() * 900) + 50);
+        var y = Math.floor((Math.random() * 500) + 50);
+        (window.drawings[Math.floor((Math.random() * 3))])(x, y);
+    }
+}
+
 
 function start() {
     document.getElementById("start_page").style.display = "none";
@@ -43,9 +55,9 @@ function start() {
     ingame_time = 60;
     // Call the drawCanvas function every second (every 1000 milliseconds)
     // in order to decrement the game timer.
-    setInterval(drawCanvas, 1000);
-    }
-    
+    drawCanvas();
+    drawSpaceObjects();
+    setInterval(drawTimer, 1000);
 }
 
 /*
@@ -62,15 +74,21 @@ function nextLevel() {
  *
  */
 function drawCanvas() {
-    ctx.clearRect(0, 0, 1000, 640);
-    ctx.fillStyle = "#000000";
-    ctx.fillRect(0, 40, 1000, 640);
+    //ctx.clearRect(0, 0, 1000, 40);
+    //ctx.fillStyle = "#000000";
+    //ctx.fillRect(0, 40, 1000, 640);
     ctx.fillStyle = "#FFFFFF";
     ctx.font = "1em Montserrat";
     ctx.textAlign = "center";
     ctx.fillText("Level " + game_level, 50, 30);
     ctx.fillText("Score: " + game_score, 300, 30);
     ctx.fillText("Pause", 700, 30);
+}
+
+function drawTimer() {
+    ctx.fillStyle = "#FFFFFF";
+    ctx.clearRect(850, 0, 1000, 40);
+    ctx.font = "1em Montserrat";
     timer();
     ctx.fillText(window.ingame_time, 900, 30);
 }
@@ -78,7 +96,7 @@ function drawCanvas() {
 /*
  * Draw spacecraft.
  */
-function Spaceship(x, y) {
+function spaceship(x, y) {
 	ctx.beginPath();
 	//Main body
     ctx.moveTo(x+10, y+20);
@@ -105,8 +123,23 @@ function Spaceship(x, y) {
 }
 
 //Draw planet with rings
-function Planet(x, y) {
+function planet(x, y) {
 	var mid_offset = 25;
+    
+	//Draw upper half of planet and clip
+	ctx.save();
+	ctx.beginPath();
+	ctx.arc(x+mid_offset, y+mid_offset, 20, 0, Math.PI);
+	ctx.clip;
+	//Set gradient and fill
+	var gradient = ctx.createLinearGradient(x, y, x, y+50);
+    gradient.addColorStop(0,"red");
+    gradient.addColorStop(1/2, '#F4A460');
+    gradient.addColorStop(1,"gray");
+    ctx.fillStyle = gradient;
+	ctx.stroke();
+	ctx.fill();
+	
 	ctx.beginPath();
 	//Ring #1
 	var width = 40;
@@ -144,7 +177,6 @@ function Planet(x, y) {
 			x+mid_offset-width, y+mid_offset+height,
 			x+mid_offset-width, y+mid_offset-height,
 			x+mid_offset, y+mid_offset-height);
-	
 	ctx.stroke();	
 	//Draw main body
 	ctx.beginPath();
@@ -154,26 +186,49 @@ function Planet(x, y) {
     var gradient = ctx.createLinearGradient(x, y, x+50, y+50);
     gradient.addColorStop(0,"red");
     gradient.addColorStop(1,"gray");
+	ctx.stroke();	
+	//Draw lower half of planet
+	ctx.restore();
+	ctx.beginPath();
+	ctx.arc(x+mid_offset, y+mid_offset, 20, Math.PI, 2*Math.PI);
+	ctx.stroke();
     ctx.fillStyle = gradient;
     ctx.fill();
 }
 
-function Moon(x, y) {
+function moon(x, y) {
 	ctx.beginPath();
 	ctx.arc(x+25, y+25, 20, 1.75*Math.PI, 1.25*Math.PI);
 	ctx.stroke();
     /* 
+=======
+	//Outer crescent
+	ctx.beginPath();
+	ctx.arc(x+25, y+25, 20, 1.2*Math.PI, 0.8*Math.PI);
+	ctx.stroke();
+	     
+>>>>>>> f0f008e5412cb8c3b1b4c5415d64be7f02061fc4
     var gradient = ctx.createLinearGradient(x, y, x+50, y+50);
     gradient.addColorStop(0,"white");
     gradient.addColorStop(1,"gray");
     ctx.fillStyle = gradient;
     ctx.fill();
+<<<<<<< HEAD
     */
 	ctx.closePath();
 	ctx.beginPath();
 	ctx.arc(x+25, y+16, 15, 1.9*Math.PI, 1.1*Math.PI);
 	
 	ctx.stroke();
+	ctx.closePath();
+	//Inner crescent
+	ctx.beginPath();
+	ctx.arc(x+17, y+25, 15, 1.3*Math.PI, 0.7*Math.PI);
+	ctx.stroke();
+	gradient = ctx.createLinearGradient(x,y,x+50, y+50);
+	gradient.addColorStop(0, "white");
+	ctx.fillStyle = gradient;
+	ctx.fill();
 }
 
 /* Potentially necessary functions.
